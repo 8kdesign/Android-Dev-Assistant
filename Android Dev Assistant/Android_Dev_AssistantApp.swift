f47b1282fa759f35259e56d7458ffd604b32585c
8kdesign <58220142+8kdesign@users.noreply.github.com>
2026-01-31 07:55:23 +0800

    Initial Commit

---
+++ Android Dev Assistant/Android_Dev_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Android_Dev_AssistantApp.swift
+//  Android Dev Assistant
+//
+//  Created by Chong Wen Hao on 31/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Android_Dev_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Android Dev Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Android Dev Assistant
+//
+//  Created by Chong Wen Hao on 31/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
