fe918f3268e798e2f153e290e51367b0ac8f7a87
8kdesign <58220142+8kdesign@users.noreply.github.com>
2026-02-21 08:35:48 +0800

    Added parent an child selection

--- Android Dev Assistant/UI/MainPanel/LayoutAnalyzer/ComponentInfoView.swift
+++ Android Dev Assistant/UI/MainPanel/LayoutAnalyzer/ComponentInfoView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ComponentInfoView: View {
     
+    @EnvironmentObject var toastHelper: ToastHelper
     @EnvironmentObject var analyzeScreenHelper: AnalyzeScreenHelper
     @State var component: ComponentItem? = nil
     @State var parentComponent: ComponentItem? = nil
+    @State var childrenComponents: [ComponentItem] = []
 
     var body: some View {
         Group {
@@ -29,6 +31,16 @@ struct ComponentInfoView: View {
             }
         }.onReceive(analyzeScreenHelper.$selectedComponent) { component in
             self.component = component
+            if let parentId = component?.parent {
+                self.parentComponent = analyzeScreenHelper.layout.components[parentId]
+            } else {
+                self.parentComponent = nil
+            }
+            if let childrenIds = component?.children {
+                self.childrenComponents = childrenIds.compactMap { analyzeScreenHelper.layout.components[$0] }
+            } else {
+                self.childrenComponents = []
+            }
         }
     }
     
@@ -37,9 +49,9 @@ struct ComponentInfoView: View {
             if let component {
                 LazyVStack(spacing: 0) {
                     Divider()
-                    DataRow(key: "Id", value: component.resourceId)
+                    DataRow(key: "Id", value: component.resourceId) { copy(component.resourceId) }
                     Divider()
-                    DataRow(key: "Accessibility Class", value: component.accessibilityClass)
+                    DataRow(key: "Accessibility Class", value: component.accessibilityClass) { copy(component.accessibilityClass) }
                     Divider()
                     if let boundsDp = component.boundsDp {
                         DataRow(key: "Size", value: "w:\(component.bounds.width) [\(String(format: "%.1f", boundsDp.width))dp], h: \(component.bounds.height) [\(String(format: "%.1f", boundsDp.height))dp]")
@@ -69,23 +81,31 @@ struct ComponentInfoView: View {
                         Divider()
                     }
                     if let text = component.text, !text.isEmpty {
-                        DataRow(key: "Text", value: text)
+                        DataRow(key: "Text", value: text) { copy(text) }
                         Divider()
                     }
                     if let hint = component.hint, !hint.isEmpty {
-                        DataRow(key: "Hint", value: hint)
+                        DataRow(key: "Hint", value: hint) { copy(hint) }
                         Divider()
                     }
                     if let contentDescription = component.contentDescription, !contentDescription.isEmpty {
-                        DataRow(key: "Content Description", value: contentDescription)
+                        DataRow(key: "Content Description", value: contentDescription) { copy(contentDescription) }
                         Divider()
                     }
+                    DataRow(key: "Parent", value: parentComponent?.getShortLabel() ?? "") {
+                        if let parentComponent {
+                            analyzeScreenHelper.addTab(component: parentComponent, needSet: true)
+                        }
+                    }
+                    Divider()
+                    ChildrenRow()
+                    Divider()
                 }
             }
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
     
-    private func DataRow(key: String, value: String) -> some View {
+    private func DataRow(key: LocalizedStringResource, value: String, action: (() -> ())? = nil) -> some View {
         HStack(alignment: .top, spacing: 0) {
             Text(key)
                 .frame(width: 80, alignment: .leading)
@@ -102,6 +122,41 @@ struct ComponentInfoView: View {
                 .foregroundColor(.white)
                 .opacity(0.7)
                 .padding(.all, 5)
+                .background(.white.opacity(0.00001))
+                .onTapGesture {
+                    action?()
+                }.hoverOpacity(action == nil ? 1 : HOVER_OPACITY)
+        }.frame(maxWidth: .infinity)
+    }
+    
+    private func ChildrenRow() -> some View {
+        HStack(alignment: .top, spacing: 0) {
+            Text("Children")
+                .frame(width: 80, alignment: .leading)
+                .multilineTextAlignment(.leading)
+                .foregroundStyle(.white)
+                .foregroundColor(.white)
+                .opacity(0.7)
+                .padding(.all, 5)
+            Divider()
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(childrenComponents) { component in
+                    Text(component.getShortLabel())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .multilineTextAlignment(.leading)
+                        .foregroundStyle(.white)
+                        .foregroundColor(.white)
+                        .opacity(0.7)
+                        .padding(.all, 5)
+                        .background(.white.opacity(0.00001))
+                        .onTapGesture {
+                            analyzeScreenHelper.addTab(component: component, needSet: true)
+                        }.hoverOpacity()
+                    if component != childrenComponents.last {
+                        Divider()
+                    }
+                }
+            }.frame(maxWidth: .infinity)
         }.frame(maxWidth: .infinity)
     }
     
@@ -110,6 +165,15 @@ struct ComponentInfoView: View {
             
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color(white: 0.12))
+    }
+    
+}
+
+extension ComponentInfoView {
+    
+    private func copy(_ string: String) {
+        copyToClipboard(string as NSString)
+        toastHelper.addToast("Copied to clipboard", style: .clipboard)
     }
     
 }
