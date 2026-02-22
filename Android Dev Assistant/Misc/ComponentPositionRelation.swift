50e3f9fbc40226c7f05997f62200c05c9c0ce673
8kdesign <58220142+8kdesign@users.noreply.github.com>
2026-02-22 21:28:59 +0800

    Simplify logic

--- Android Dev Assistant/Misc/ComponentPositionRelation.swift
+++ Android Dev Assistant/Misc/ComponentPositionRelation.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 enum ComponentPositionRelation {
     case identical
-    case noOverlap(
+    case nonIdentical(
         xIntersectType: AxisIntersectType,
         yIntersectType: AxisIntersectType,
         left: CGFloat,
@@ -18,37 +18,7 @@ enum ComponentPositionRelation {
         top: CGFloat,
         bottom: CGFloat
     )
-    case mainContainsOther(left: CGFloat, right: CGFloat, top: CGFloat, bottom: CGFloat)
-    case otherContainsMain(left: CGFloat, right: CGFloat, top: CGFloat, bottom: CGFloat)
-    case partialOverlapHorizontal( // start and end are outside, top and bottom are inside
-        left: CGFloat,
-        right: CGFloat,
-        top: CGFloat,
-        bottom: CGFloat
-    )
-    case partialOverlapVertical( // start and end are inside, top and bottom are outside
-        left: CGFloat,
-        right: CGFloat,
-        top: CGFloat,
-        bottom: CGFloat
-    )
-    case partialOverlapCorner(
-        corner: Corner,
-        left: CGFloat,
-        right: CGFloat,
-        top: CGFloat,
-        bottom: CGFloat
-    )
-    case partialOverlapSide(
-        side: Side,
-        left: CGFloat, // if side is top/bottom, check width to determine if inner/outer
-        right: CGFloat, // if side is top/bottom, check width to determine if inner/outer
-        top: CGFloat, // if side is left/right, check height to determine if inner/outer
-        bottom: CGFloat, // if side is left/right, check height to determine if inner/outer
-        mainSize: CGSize,
-        otherSize: CGSize
-    )
-    
+
     enum AxisIntersectType {
         case negative
         case partialNegative
@@ -58,31 +28,10 @@ enum ComponentPositionRelation {
         case outside
     }
     
-    enum Corner {
-        case topLeft
-        case topRight
-        case bottomLeft
-        case bottomRight
-    }
-
-    enum Side {
-        case left
-        case right
-        case top
-        case bottom
-    }
-    
     static func getPositionRelation(mainBounds: CGRect, otherBounds: CGRect) -> ComponentPositionRelation {
         if mainBounds == otherBounds {
             return .identical
         }
-        if !mainBounds.intersects(otherBounds) {
-            return processNonIntersecting(mainBounds: mainBounds, otherBounds: otherBounds)
-        }
-        return processIntersecting(mainBounds: mainBounds, otherBounds: otherBounds)
-    }
-    
-    private static func processNonIntersecting(mainBounds: CGRect, otherBounds: CGRect) -> ComponentPositionRelation {
         var xIntersectType: AxisIntersectType
         var leftOffset: CGFloat = 0
         var rightOffset: CGFloat = 0
@@ -122,7 +71,7 @@ enum ComponentPositionRelation {
             yIntersectType = .inside
             topOffset = abs(mainBounds.minY - otherBounds.minY)
             bottomOffset = abs(mainBounds.maxY - otherBounds.maxY)
-        } else if mainBounds.minY > otherBounds.minY && mainBounds.maxY < otherBounds.maxY {
+        } else if mainBounds.minY >= otherBounds.minY && mainBounds.maxY <= otherBounds.maxY {
             yIntersectType = .outside
             topOffset = abs(mainBounds.minY - otherBounds.minY)
             bottomOffset = abs(mainBounds.maxY - otherBounds.maxY)
@@ -135,140 +84,8 @@ enum ComponentPositionRelation {
             topOffset = abs(mainBounds.minY - otherBounds.minY)
             bottomOffset = abs(mainBounds.maxY - otherBounds.minY)
         }
-        return .noOverlap(xIntersectType: xIntersectType, yIntersectType: yIntersectType,
+        return .nonIdentical(xIntersectType: xIntersectType, yIntersectType: yIntersectType,
                           left: leftOffset, right: rightOffset, top: topOffset, bottom: bottomOffset)
     }
 
-    private static func processIntersecting(mainBounds: CGRect, otherBounds: CGRect) -> ComponentPositionRelation {
-        let topTopDiff = mainBounds.minY - otherBounds.minY
-        let bottomBottomDiff = mainBounds.maxY - otherBounds.maxY
-        let leftLeftDiff = mainBounds.minX - otherBounds.minX
-        let rightRightDiff = mainBounds.maxX - otherBounds.maxX
-        if mainBounds.contains(otherBounds) {
-            return .mainContainsOther(
-                left: abs(leftLeftDiff),
-                right: abs(rightRightDiff),
-                top: abs(topTopDiff),
-                bottom: abs(bottomBottomDiff)
-            )
-        }
-        if otherBounds.contains(mainBounds) {
-            return .otherContainsMain(
-                left: abs(leftLeftDiff),
-                right: abs(rightRightDiff),
-                top: abs(topTopDiff),
-                bottom: abs(bottomBottomDiff)
-            )
-        }
-        
-        let topBottomDiff = mainBounds.minY - otherBounds.maxY
-        let bottomTopDiff = mainBounds.maxY - otherBounds.minY
-        let leftRightDiff = mainBounds.minX - otherBounds.maxX
-        let rightLeftDiff = mainBounds.maxX - otherBounds.minX
-        
-        let isLeftInside = mainBounds.minX <= otherBounds.minX && mainBounds.maxX >= otherBounds.minX
-        let isRightInside = mainBounds.minX <= otherBounds.maxX && mainBounds.maxX >= otherBounds.maxX
-        let isTopInside = mainBounds.minY <= otherBounds.minY && mainBounds.maxY >= otherBounds.minY
-        let isBottomInside = mainBounds.minY <= otherBounds.maxY && mainBounds.maxY >= otherBounds.maxY
-        var horizontalInsideCount = 0
-        var verticalInsideCount = 0
-        if isLeftInside { horizontalInsideCount += 1 }
-        if isRightInside { horizontalInsideCount += 1 }
-        if isTopInside { verticalInsideCount += 1 }
-        if isBottomInside { verticalInsideCount += 1 }
-        
-        if horizontalInsideCount == 1 {
-            switch verticalInsideCount {
-            case 1: // corner
-                let isLeftCorner = isRightInside
-                let isTopCorner = isBottomInside
-                let corner: Corner = isLeftCorner ? isTopCorner ? .topLeft : .bottomLeft : isTopCorner ? .topRight : .bottomRight
-                return .partialOverlapCorner(
-                    corner: corner,
-                    left: abs(leftLeftDiff),
-                    right: abs(rightRightDiff),
-                    top: abs(topTopDiff),
-                    bottom: abs(bottomBottomDiff)
-                )
-            case 2: // side, top and bottom inside
-                let isLeftSide = isRightInside
-                return .partialOverlapSide(
-                    side: isLeftSide ? .left : .right,
-                    left: abs(isLeftSide ? leftLeftDiff : rightLeftDiff),
-                    right: abs(isLeftSide ? leftRightDiff : rightRightDiff),
-                    top: abs(topTopDiff),
-                    bottom: abs(bottomBottomDiff),
-                    mainSize: mainBounds.size,
-                    otherSize: otherBounds.size
-                )
-            default: // side, top and bottom outside
-                let isLeftSide = isRightInside
-                return .partialOverlapSide(
-                    side: isLeftSide ? .left : .right,
-                    left: abs(isLeftSide ? leftLeftDiff : rightLeftDiff),
-                    right: abs(isLeftSide ? leftRightDiff : rightRightDiff),
-                    top: abs(topTopDiff),
-                    bottom: abs(bottomBottomDiff),
-                    mainSize: mainBounds.size,
-                    otherSize: otherBounds.size
-                )
-            }
-        } else if horizontalInsideCount == 2 {
-            switch verticalInsideCount {
-            case 1: // side, left and right inside
-                let isTopSide = isBottomInside
-                return .partialOverlapSide(
-                    side: isTopSide ? .top : .bottom,
-                    left: abs(leftLeftDiff),
-                    right: abs(rightRightDiff),
-                    top: abs(isTopSide ? topTopDiff : bottomTopDiff),
-                    bottom: abs(isTopSide ? topBottomDiff : bottomBottomDiff),
-                    mainSize: mainBounds.size,
-                    otherSize: otherBounds.size
-                )
-            case 2: // inside (should have been caught earlier)
-                return .mainContainsOther(
-                    left: abs(leftLeftDiff),
-                    right: abs(rightRightDiff),
-                    top: abs(topTopDiff),
-                    bottom: abs(bottomBottomDiff)
-                )
-            default: // vertical
-                return .partialOverlapVertical(
-                    left: abs(leftLeftDiff),
-                    right: abs(rightRightDiff),
-                    top: abs(topTopDiff),
-                    bottom: abs(bottomBottomDiff)
-                )
-            }
-        } else {
-            switch verticalInsideCount {
-            case 1: // side, left and right outside
-                let isTopSide = isBottomInside
-                return .partialOverlapSide(
-                    side: isTopSide ? .top : .bottom,
-                    left: abs(leftLeftDiff),
-                    right: abs(rightRightDiff),
-                    top: abs(isTopSide ? topTopDiff : bottomTopDiff),
-                    bottom: abs(isTopSide ? topBottomDiff : bottomBottomDiff),
-                    mainSize: mainBounds.size,
-                    otherSize: otherBounds.size
-                )
-            case 2: // horizontal
-                return .partialOverlapHorizontal(
-                    left: abs(leftLeftDiff),
-                    right: abs(rightRightDiff),
-                    top: abs(topTopDiff),
-                    bottom: abs(bottomBottomDiff)
-                )
-            default: // outside (should have been caught earlier)
-                return .otherContainsMain(
-                    left: abs(leftLeftDiff),
-                    right: abs(rightRightDiff),
-                    top: abs(topTopDiff),
-                    bottom: abs(bottomBottomDiff)
-                )
-            }
-        }
-    }
 }
--- Android Dev Assistant/UI/MainPanel/LayoutAnalyzer/SpacingComparatorView.swift
+++ Android Dev Assistant/UI/MainPanel/LayoutAnalyzer/SpacingComparatorView.swift
@@ -75,200 +75,54 @@ extension SpacingComparatorView {
     
     private func getOtherRect(size: CGSize, mainRect: CGRect, relation: ComponentPositionRelation?) -> CGRect? {
         switch relation {
-        case .noOverlap(let xIntersectType, let yIntersectType, let left, let right, let top, let bottom):
-            return getNoOverlapRect(size: size, mainRect: mainRect,
-                          xIntersectType: xIntersectType, yIntersectType: yIntersectType,
-                          left: left, right: right, top: top, bottom: bottom)
-        case .mainContainsOther(let left, let right, let top, let bottom):
-            return CGRect(
-                x: mainRect.minX + (left > 0 ? 20 : 0),
-                y: mainRect.minY + (top > 0 ? 20 : 0),
-                width: mainRect.width - (left > 0 ? 20 : 0) - (right > 0 ? 20 : 0),
-                height: mainRect.height - (top > 0 ? 20 : 0) - (bottom > 0 ? 20 : 0)
-            )
-        case .otherContainsMain(let left, let right, let top, let bottom):
-            return CGRect(
-                x: mainRect.minX - (left > 0 ? (BOX_WIDTH / 2) : 0),
-                y: mainRect.minY - (top > 0 ? (BOX_WIDTH / 2) : 0),
-                width: mainRect.width + (left > 0 ? (BOX_WIDTH / 2) : 0) + (right > 0 ? (BOX_WIDTH / 2) : 0),
-                height: mainRect.height + (top > 0 ? (BOX_WIDTH / 2) : 0) + (bottom > 0 ? (BOX_WIDTH / 2) : 0)
-            )
-        case .partialOverlapHorizontal(_, _, _ ,_):
-            return CGRect(
-                x: mainRect.minX - (BOX_WIDTH / 2),
-                y: mainRect.minY + (BOX_WIDTH / 2),
-                width: mainRect.width + BOX_WIDTH,
-                height: mainRect.height - BOX_WIDTH
-            )
-        case .partialOverlapVertical(_, _, _, _):
-            return CGRect(
-                x: mainRect.minX + (BOX_WIDTH / 2),
-                y: mainRect.minY - (BOX_WIDTH / 2),
-                width: mainRect.width - BOX_WIDTH,
-                height: mainRect.height + BOX_WIDTH
-            )
-        case .partialOverlapCorner(let corner, let left, let right, let top, let bottom):
-            return getOverlapCornerRect(size: size, mainRect: mainRect, corner: corner,
-                                    left: left, right: right, top: top, bottom: bottom)
-        case .partialOverlapSide(let side, let left, let right, let top, let bottom, let mainSize, let otherSize):
-            return getOverlapSideRect(size: size, mainRect: mainRect, side: side,
-                                  left: left, right: right, top: top, bottom: bottom,
-                                  mainSize: mainSize, otherSize: otherSize)
+        case .nonIdentical(let xIntersectType, let yIntersectType, let left, let right, let top, let bottom):
+            var x1Position: CGFloat = 0
+            var x2Position: CGFloat = 0
+            switch xIntersectType {
+            case .negative:
+                x2Position = mainRect.minX - (left > 0 ? (BOX_WIDTH / 2) : 0)
+                x1Position = x2Position - BOX_WIDTH
+            case .partialNegative:
+                x2Position = mainRect.minX + (left > 0 ? (BOX_WIDTH / 2) : 0)
+                x1Position = x2Position - BOX_WIDTH
+            case .inside:
+                x1Position = mainRect.minX + (left > 0 ? 20 : 0)
+                x2Position = mainRect.maxX - (right > 0 ? 20 : 0)
+            case .partialPositive:
+                x2Position = mainRect.maxX + (right > 0 ? (BOX_WIDTH / 2) : 0)
+                x1Position = x2Position - BOX_WIDTH
+            case .positive:
+                x1Position = mainRect.maxX + (right > 0 ? (BOX_WIDTH / 2) : 0)
+                x2Position = x1Position + BOX_WIDTH
+            case .outside:
+                x1Position = mainRect.minX - (left > 0 ? (BOX_WIDTH / 2) : 0)
+                x2Position = mainRect.maxX + (right > 0 ? (BOX_WIDTH / 2) : 0)
+            }
+            var y1Position: CGFloat = 0
+            var y2Position: CGFloat = 0
+            switch yIntersectType {
+            case .negative:
+                y2Position = mainRect.minY - (top > 0 ? (BOX_WIDTH / 2) : 0)
+                y1Position = y2Position - BOX_WIDTH
+            case .partialNegative:
+                y2Position = mainRect.minY + (top > 0 ? (BOX_WIDTH / 2) : 0)
+                y1Position = y2Position - BOX_WIDTH
+            case .inside:
+                y1Position = mainRect.minY + (top > 0 ? 20 : 0)
+                y2Position = mainRect.maxY - (bottom > 0 ? 20 : 0)
+            case .partialPositive:
+                y2Position = mainRect.maxY + (bottom > 0 ? (BOX_WIDTH / 2) : 0)
+                y1Position = y2Position - BOX_WIDTH
+            case .positive:
+                y1Position = mainRect.maxY + (bottom > 0 ? (BOX_WIDTH / 2) : 0)
+                y2Position = y1Position + BOX_WIDTH
+            case .outside:
+                y1Position = mainRect.minY - (top > 0 ? (BOX_WIDTH / 2) : 0)
+                y2Position = mainRect.maxY + (bottom > 0 ? (BOX_WIDTH / 2) : 0)
+            }
+            return CGRect(x: x1Position, y: y1Position, width: (x2Position - x1Position), height: (y2Position - y1Position))
         case nil, .identical:
             return nil
-        }
-    }
-    
-    private func getNoOverlapRect(
-        size: CGSize,
-        mainRect: CGRect,
-        xIntersectType: ComponentPositionRelation.AxisIntersectType,
-        yIntersectType: ComponentPositionRelation.AxisIntersectType,
-        left: CGFloat,
-        right: CGFloat,
-        top: CGFloat,
-        bottom: CGFloat
-    ) -> CGRect {
-        var x1Position: CGFloat = 0
-        var x2Position: CGFloat = 0
-        switch xIntersectType {
-        case .negative:
-            x2Position = mainRect.minX - (left > 0 ? (BOX_WIDTH / 2) : 0)
-            x1Position = x2Position - BOX_WIDTH
-        case .partialNegative:
-            x2Position = mainRect.minX + (left > 0 ? (BOX_WIDTH / 2) : 0)
-            x1Position = x2Position - BOX_WIDTH
-        case .inside:
-            x1Position = mainRect.minX + (left > 0 ? 20 : 0)
-            x2Position = mainRect.maxX - (right > 0 ? 20 : 0)
-        case .partialPositive:
-            x2Position = mainRect.maxX + (right > 0 ? (BOX_WIDTH / 2) : 0)
-            x1Position = x2Position - BOX_WIDTH
-        case .positive:
-            x1Position = mainRect.maxX + (right > 0 ? (BOX_WIDTH / 2) : 0)
-            x2Position = x1Position + BOX_WIDTH
-        case .outside:
-            x1Position = mainRect.minX - (left > 0 ? (BOX_WIDTH / 2) : 0)
-            x2Position = mainRect.maxX + (right > 0 ? (BOX_WIDTH / 2) : 0)
-        }
-        var y1Position: CGFloat = 0
-        var y2Position: CGFloat = 0
-        switch yIntersectType {
-        case .negative:
-            y2Position = mainRect.minY - (top > 0 ? (BOX_WIDTH / 2) : 0)
-            y1Position = y2Position - BOX_WIDTH
-        case .partialNegative:
-            y2Position = mainRect.minY + (top > 0 ? (BOX_WIDTH / 2) : 0)
-            y1Position = y2Position - BOX_WIDTH
-        case .inside:
-            y1Position = mainRect.minY + (top > 0 ? 20 : 0)
-            y2Position = mainRect.maxY - (bottom > 0 ? 20 : 0)
-        case .partialPositive:
-            y2Position = mainRect.maxY + (bottom > 0 ? (BOX_WIDTH / 2) : 0)
-            y1Position = y2Position - BOX_WIDTH
-        case .positive:
-            y1Position = mainRect.maxY + (bottom > 0 ? (BOX_WIDTH / 2) : 0)
-            y2Position = y1Position + BOX_WIDTH
-        case .outside:
-            y1Position = mainRect.minY - (top > 0 ? (BOX_WIDTH / 2) : 0)
-            y2Position = mainRect.maxY + (bottom > 0 ? (BOX_WIDTH / 2) : 0)
-        }
-        return CGRect(x: x1Position, y: y1Position, width: (x2Position - x1Position), height: (y2Position - y1Position))
-    }
-    
-    private func getOverlapCornerRect(
-        size: CGSize,
-        mainRect: CGRect,
-        corner: ComponentPositionRelation.Corner,
-        left: CGFloat,
-        right: CGFloat,
-        top: CGFloat,
-        bottom: CGFloat,
-    ) -> CGRect {
-        let leftPadding: CGFloat = left > 0 ? (BOX_WIDTH / 2) : 0
-        let rightPadding: CGFloat = right > 0 ? (BOX_WIDTH / 2) : 0
-        let topPadding: CGFloat = top > 0 ? (BOX_WIDTH / 2) : 0
-        let bottomPadding: CGFloat = bottom > 0 ? (BOX_WIDTH / 2) : 0
-        switch corner {
-        case .topLeft:
-            return CGRect(
-                x: mainRect.minX - leftPadding,
-                y: mainRect.minY - topPadding,
-                width: mainRect.width - rightPadding + leftPadding,
-                height: mainRect.height - bottomPadding + topPadding
-            )
-        case .topRight:
-            return CGRect(
-                x: mainRect.minX + leftPadding,
-                y: mainRect.minY - topPadding,
-                width: mainRect.width + rightPadding - leftPadding,
-                height: mainRect.height - bottomPadding + topPadding
-            )
-        case .bottomLeft:
-            return CGRect(
-                x: mainRect.minX - leftPadding,
-                y: mainRect.minY + topPadding,
-                width: mainRect.width - rightPadding + leftPadding,
-                height: mainRect.height + bottomPadding - topPadding
-            )
-        case .bottomRight:
-            return CGRect(
-                x: mainRect.minX + leftPadding,
-                y: mainRect.minY + topPadding,
-                width: mainRect.width + rightPadding - leftPadding,
-                height: mainRect.height + bottomPadding - topPadding
-            )
-        }
-    }
-    
-    private func getOverlapSideRect(
-        size: CGSize,
-        mainRect: CGRect,
-        side: ComponentPositionRelation.Side,
-        left: CGFloat, // if side is top/bottom, check width to determine if inner/outer
-        right: CGFloat, // if side is top/bottom, check width to determine if inner/outer
-        top: CGFloat, // if side is left/right, check height to determine if inner/outer
-        bottom: CGFloat, // if side is left/right, check height to determine if inner/outer
-        mainSize: CGSize,
-        otherSize: CGSize
-    ) -> CGRect {
-        switch side {
-        case .left:
-            let topOffset: CGFloat = top > 0 ? mainSize.height > otherSize.height ? (BOX_WIDTH / 2) : -(BOX_WIDTH / 2)  : 0
-            let bottomOffset: CGFloat = bottom > 0 ? mainSize.height > otherSize.height ? -(BOX_WIDTH / 2) : (BOX_WIDTH / 2) : 0
-            return CGRect(
-                x: mainRect.minX - (BOX_WIDTH / 2),
-                y: mainRect.minY + topOffset,
-                width: BOX_WIDTH,
-                height: mainRect.height - topOffset + bottomOffset
-            )
-        case .right:
-            let topOffset: CGFloat = top > 0 ? mainSize.height > otherSize.height ? (BOX_WIDTH / 2) : -(BOX_WIDTH / 2)  : 0
-            let bottomOffset: CGFloat = bottom > 0 ? mainSize.height > otherSize.height ? -(BOX_WIDTH / 2) : (BOX_WIDTH / 2) : 0
-            return CGRect(
-                x: mainRect.maxX - (BOX_WIDTH / 2),
-                y: mainRect.minY + topOffset,
-                width: BOX_WIDTH,
-                height: mainRect.height - topOffset + bottomOffset
-            )
-        case .top:
-            let leftOffset: CGFloat = left > 0 ? mainSize.width > otherSize.width ? (BOX_WIDTH / 2) : -(BOX_WIDTH / 2)  : 0
-            let rightOffset: CGFloat = right > 0 ? mainSize.width > otherSize.width ? -(BOX_WIDTH / 2) : (BOX_WIDTH / 2) : 0
-            return CGRect(
-                x: mainRect.minX + leftOffset,
-                y: mainRect.minY - (BOX_WIDTH / 2),
-                width: mainRect.width - leftOffset + rightOffset,
-                height: BOX_WIDTH
-            )
-        case .bottom:
-            let leftOffset: CGFloat = left > 0 ? mainSize.width > otherSize.width ? (BOX_WIDTH / 2) : -(BOX_WIDTH / 2)  : 0
-            let rightOffset: CGFloat = right > 0 ? mainSize.width > otherSize.width ? -(BOX_WIDTH / 2) : (BOX_WIDTH / 2) : 0
-            return CGRect(
-                x: mainRect.minX + leftOffset,
-                y: mainRect.maxY - (BOX_WIDTH / 2),
-                width: mainRect.width - leftOffset + rightOffset,
-                height: BOX_WIDTH
-            )
         }
     }
     
